7f09f2db808dc3c6cd0219126ee2e4f718a322a5
Ariq Khoiri <qq.khoiri@gmail.com>
2024-03-28 20:31:23 +0700

    transform to arrow function

--- topic1_function/hellownyaw.js
+++ topic1_function/hellownyaw.js
@@ -3,3 +3,10 @@ function helloMeow(name, breed, age) {
 }
 
 console.log(helloMeow("Hikaru", "Persia", 2))
+
+// to arrow
+const helloNyan = (name, breed, age) => {
+    return `Hellow my name is neko, ${name}, breed ${breed}, age ${age}`
+}
+
+console.log(helloNyan("Ai", "Persia", 1))
