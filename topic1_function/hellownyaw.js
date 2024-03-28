17f9854481ebaa5e1631b0edadb815d0ea89f264
Ariq Khoiri <qq.khoiri@gmail.com>
2024-03-28 20:32:59 +0700

    add anon function

--- topic1_function/hellownyaw.js
+++ topic1_function/hellownyaw.js
@@ -10,3 +10,10 @@ const helloNyan = (name, breed, age) => {
 }
 
 console.log(helloNyan("Ai", "Persia", 1))
+
+// to anon
+const helloNeko = function (name, breed, age) {
+    return `Hellow my name is neko, ${name}, breed ${breed}, age ${age}`
+}
+
+console.log(helloNeko("Persia juliet", "Persia", 16))
